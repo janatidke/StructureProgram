f4e060820e70fae4a746b2692509ca353e16bb64
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-08-31 23:12:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StructureProgram
+{
+    class Program
+    {
+        public struct Student
+        {
+            public string Name;
+            public string Course_name;
+            public int stud_id;
+            public int reg_id;
+
+        }
+        static void Main(string[] args)
+        {
+            Student s1;
+            s1.Name = "jana";
+            s1.Course_name = "Csharp";
+            s1.stud_id = 56;
+            s1.reg_id = 67667;
+
+            Console.WriteLine("Student Details are:");
+            Console.WriteLine("\nName{0},\nCourse_name{1},\nstu_id{2},\nreg_id{3}", s1.Name, s1.Course_name, s1.stud_id, s1.reg_id);
+        }
+    }
+}
